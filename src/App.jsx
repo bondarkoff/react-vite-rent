42174a7f33f82c545bda6a7336118b8ba9da1344
Yehor Bondarkov <yehor.bondarkoff@gmail.com>
2023-04-15 22:59:04 +0300

    404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Favorite from './components/pages/Favorite';
 import Profile from './components/pages/Profile';
 import Settings from './components/pages/Settings';
 import Notifications from './components/pages/Notifications';
+import NotFound from './components/pages/NotFound';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Route path='settings' exact element={<Settings />} />
             <Route path='profile' exact element={<Profile />} />
             <Route path='notifications' exact element={<Notifications />} />
+            <Route path='*' element={<NotFound />} />
         </Routes>
     );
 }
