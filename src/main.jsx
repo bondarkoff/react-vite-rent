6d56d14d6d6fad0c145b9306d6932e0b83f327ed
Yehor Bondarkov <yehor.bondarkoff@gmail.com>
2023-04-27 12:02:40 +0300

    added _variables.scss

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 
 import './assets/styles/global.scss';
+
 import 'css-shortcut';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
